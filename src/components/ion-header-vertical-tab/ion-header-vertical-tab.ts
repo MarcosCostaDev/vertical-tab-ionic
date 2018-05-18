c1b977baa22afa7abccae5910719e6ea92148814
Marcos Costa <marcos.lcosta@live.com>
2018-05-18 16:30:28 -0300

    add components

---
+++ src/components/ion-header-vertical-tab/ion-header-vertical-tab.ts
@@ -0,0 +1,17 @@
+import { IonVerticalTabComponent } from '..';
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the IonHeaderVerticalTabComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'ion-header-vertical-tab',
+  templateUrl: 'ion-header-vertical-tab.html'
+})
+export class IonHeaderVerticalTabComponent extends IonVerticalTabComponent {
+
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
-export * from './my-module.module';
-export * from './components/my-component';
-export * from './providers/my-provider';
+export * from './components';
