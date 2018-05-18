48192e4650295cccccc0d288f6d091709fec909e
Marcos Costa <marcos.lcosta@live.com>
2018-05-18 16:38:25 -0300

    update ionic-angular

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export * from './ion-vertical-tab/ion-vertical-tab';
 export * from './ion-vertical-tabs/ion-vertical-tabs';
 export * from './ion-header-vertical-tab/ion-header-vertical-tab';
 export * from './ion-footer-vertical-tab/ion-footer-vertical-tab';
+export * from './verticaltab.module';
